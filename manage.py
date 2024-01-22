4bfd408cb2a3f5e106421ff2c19254a30a07d7d9
Archy <antoine.tassez@gmail.com>
2024-01-22 13:10:50 +0100

    Mise à jour du logging suite à l'intégration gunicorn

--- manage.py
+++ manage.py
@@ -9,44 +9,6 @@ from logging.handlers import TimedRotatingFileHandler
 import sentry_sdk
 from sentry_sdk.integrations.logging import LoggingIntegration
 
-load_dotenv()
-
-log_directory = os.getenv("LOG_DIRECTORY", "logs")
-
-if not os.path.exists(log_directory):
-    os.makedirs(log_directory)
-
-# Configuration du journal local
-# Gestionnaire de fichiers rotatifs basé sur la date
-log_filename = f"logs/{datetime.datetime.now().strftime('%Y-%m-%d')}.log"
-daily_log_handler = TimedRotatingFileHandler(
-    log_filename,
-    when="midnight",
-    interval=1,
-    encoding="utf-8",
-    delay=False,
-)
-
-log_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-daily_log_handler.setFormatter(log_formatter)
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-logger.addHandler(daily_log_handler)
-
-# Intégration à Sentry :
-sentry_dsn = os.getenv("SENTRY_DSN", None)
-if sentry_dsn is not None:
-    sentry_sdk.init(
-        dsn=sentry_dsn,
-        integrations=[
-            LoggingIntegration(
-                level=logging.INFO,  # Capture info and above as breadcrumbs
-                event_level=logging.WARNING,  # Send records as events
-            ),
-        ],
-    )
-
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oc_lettings_site.settings")
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
+import logger
 
 load_dotenv()
 
